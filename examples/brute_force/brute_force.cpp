4e6912a812362f7ca514bcd048021ec211f874de
Ana Gainaru <ana.gainaru@gmail.com>
2020-05-19 17:15:33 -0400

    Update the example code

--- examples/brute_force/brute_force.cpp
+++ examples/brute_force/brute_force.cpp
@@ -10,61 +10,52 @@
  ****************************************************************************/
 
 #include <ArborX_BruteForce.hpp>
+#include <ArborX_DetailsBoundingVolumeHierarchyImpl.hpp>
 #include <ArborX_DetailsBufferOptimization.hpp>
 #include <ArborX_LinearBVH.hpp>
 
 #include <Kokkos_Core.hpp>
+#include <stdlib.h>
+#include <unistd.h>
 
-struct Dummy
-{
+struct Dummy {
+  int count;
 };
 
 using MemorySpace = Kokkos::HostSpace;
 using ExecutionSpace = Kokkos::DefaultHostExecutionSpace;
 
-namespace ArborX
-{
-namespace Traits
-{
-template <>
-struct Access<Dummy, PrimitivesTag>
-{
+namespace ArborX {
+namespace Traits {
+template <> struct Access<Dummy, PrimitivesTag> {
   using memory_space = MemorySpace;
   using size_type = typename MemorySpace::size_type;
-  inline static size_type size(Dummy const &) { return 5; }
-  KOKKOS_FUNCTION static Point get(Dummy const &, size_type i)
-  {
+  inline static size_type size(Dummy const &d) { return d.count; }
+  KOKKOS_FUNCTION static Point get(Dummy const &, size_type i) {
     return {{(float)i, (float)i, (float)i}};
   }
 };
 
-template <>
-struct Access<Dummy, PredicatesTag>
-{
+template <> struct Access<Dummy, PredicatesTag> {
   using memory_space = MemorySpace;
   using size_type = typename MemorySpace::size_type;
-  inline static size_type size(Dummy const &) { return 5; }
-  KOKKOS_FUNCTION static auto get(Dummy const &, size_type i)
-  {
-    return attach(
-        intersects(Sphere{{{(float)i, (float)i, (float)i}}, (float)i}), i);
+  inline static size_type size(Dummy const &d) { return d.count; }
+  KOKKOS_FUNCTION static auto get(Dummy const &, size_type i) {
+    return attach(intersects(Sphere{{{(float)i, (float)i, (float)i}},
+                                    KokkosExt::log((float)i + 1)}),
+                  i);
   }
 };
 } // namespace Traits
 } // namespace ArborX
 
-struct PrintfCallback
-{
+struct PrintfCallback {
   using tag = ArborX::Details::InlineCallbackTag;
   Kokkos::View<int, ExecutionSpace, Kokkos::MemoryTraits<Kokkos::Atomic>> c_;
-  PrintfCallback()
-      : c_{"counter"}
-  {
-  }
+  PrintfCallback() : c_{"counter"} {}
   template <typename Predicate, typename OutputFunctor>
   KOKKOS_FUNCTION void operator()(Predicate const &predicate, int i,
-                                  OutputFunctor const &out) const
-  {
+                                  OutputFunctor const &out) const {
     int const j = getData(predicate);
     printf("%d callback (%d,%d)\n", ++c_(), i, j);
     out(i);
@@ -72,8 +63,7 @@ struct PrintfCallback
 };
 
 template <typename T, typename... P>
-std::vector<T> view2vec(Kokkos::View<T *, P...> view)
-{
+std::vector<T> view2vec(Kokkos::View<T *, P...> view) {
   std::vector<T> vec(view.size());
   Kokkos::deep_copy(Kokkos::View<T *, Kokkos::HostSpace,
                                  Kokkos::MemoryTraits<Kokkos::Unmanaged>>(
@@ -83,8 +73,7 @@ std::vector<T> view2vec(Kokkos::View<T *, P...> view)
 }
 
 template <typename OutputView, typename OffsetView>
-void print(OutputView const out, OffsetView const offset)
-{
+void print(OutputView const out, OffsetView const offset) {
   int const n_queries = offset.extent(0) - 1;
 
   auto const h_out = view2vec(out);
@@ -96,37 +85,75 @@ void print(OutputView const out, OffsetView const offset)
       printf("%d result (%d, %d)\n", ++count, h_out[k], j);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   Kokkos::ScopeGuard guard(argc, argv);
 
+  int nqueries = 5, nprimitives = 5, nrepeats = 1;
+  int c;
+  while ((c = getopt(argc, argv, "p:q:r:")) != -1)
+    switch (c) {
+    case 'p':
+      nprimitives = atoi(optarg);
+      break;
+    case 'q':
+      nqueries = atoi(optarg);
+      break;
+    case 'r':
+      nrepeats = atoi(optarg);
+      break;
+    case '?':
+      fprintf(stderr, "Usage: %s [-p <count> -q <count>]\n", argv[0]);
+      return 1;
+    default:
+      abort();
+    }
+
+  printf("Primitives: %d\n", nprimitives);
+  printf("Predicates: %d\n", nqueries);
+
   ExecutionSpace space{};
-  Dummy primitives{};
-  Dummy predicates{};
+  Dummy primitives{nprimitives};
+  Dummy predicates{nqueries};
 
-  printf("Bounding Volume Hierarchy\n");
-  {
-    ArborX::BVH<MemorySpace> bvh{space, primitives};
+  for (int i = 0; i < nrepeats; i++) {
+    int out_count;
+    // printf("Bounding Volume Hierarchy\n");
+    {
+      Kokkos::Timer timer;
+      ArborX::BoundingVolumeHierarchy<MemorySpace> bvh{space, primitives};
 
-    Kokkos::View<int *, ExecutionSpace> indices("indices_ref", 0);
-    Kokkos::View<int *, ExecutionSpace> offset("offset_ref", 0);
-    bvh.query(space, predicates, PrintfCallback{}, indices, offset);
+      Kokkos::View<int *, ExecutionSpace> indices("indices_ref", 0);
+      Kokkos::View<int *, ExecutionSpace> offset("offset_ref", 0);
+      bvh.query(
+          space, predicates, ArborX::Details::CallbackDefaultSpatialPredicate{},
+          indices, offset,
+          ArborX::Experimental::TraversalPolicy{}.setPredicateSorting(true));
 
-    printf("print\n");
-    print(indices, offset);
+      double time = timer.seconds();
+      if (i == 0)
+        printf("Collisions: %.5f\n",
+               (float)(indices.extent(0)) / (nprimitives * nqueries));
+      printf("Time BVH: %lf\n", time);
+      // print(indices, offset);
+      out_count = indices.extent(0);
+    }
+
+    // printf("Brute Force\n");
+    {
+      Kokkos::Timer timer;
+      ArborX::BruteForce<MemorySpace> brute{space, primitives};
+
+      Kokkos::View<int *, ExecutionSpace> indices("indices", 0);
+      Kokkos::View<int *, ExecutionSpace> offset("offset", 0);
+      brute.query(space, predicates,
+                  ArborX::Details::CallbackDefaultSpatialPredicate{}, indices,
+                  offset);
+
+      double time = timer.seconds();
+      printf("Time BF: %lf\n", time);
+      // print(indices, offset);
+      ARBORX_ASSERT(out_count == indices.extent(0));
+    }
   }
-
-  printf("Brute Force\n");
-  {
-    ArborX::BruteForce<MemorySpace> brute{space, primitives};
-
-    Kokkos::View<int *, ExecutionSpace> indices("indices", 0);
-    Kokkos::View<int *, ExecutionSpace> offset("offset", 0);
-    brute.query(space, predicates, PrintfCallback{}, indices, offset);
-
-    printf("print\n");
-    print(indices, offset);
-  }
-
   return 0;
 }
